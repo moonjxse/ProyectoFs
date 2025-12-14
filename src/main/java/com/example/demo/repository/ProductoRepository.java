f20d159aa8273dff2be6ad8997780d12463a1362
Jose Diaz <josa.diazm@duocuc.cl>
2025-12-14 02:28:51 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    // Esto nos da métodos mágicos como findAll(), save(), deleteById()
+}
